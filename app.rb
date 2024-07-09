62af8d404821fc890814ce0b11d07375dec66c73
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-07-09 23:43:26 +0000

    Finalize Rock/Paper/Scissors Files

--- app.rb
+++ app.rb
@@ -4,3 +4,18 @@ require "sinatra/reloader"
 get("/") do
   erb(:home)
 end
+
+get("/rock") do
+  @computer = ["rock", "paper", "scissors"].sample
+  erb(:rock)
+end
+
+get("/paper") do
+  @computer = ["rock", "paper", "scissors"].sample
+  erb(:paper)
+end
+
+get("/scissors") do
+  @computer = ["rock", "paper", "scissors"].sample
+  erb(:scissors)
+end
